b3b9d69b42da8acbbe74eacfa238259d540c901a
Votre Nom <ummihamidou@gmail.com>
2025-05-19 00:21:02 +0100

    vl

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
 
-int main() {
-    int limit, a = 0, b = 1, next;
-    scanf("%d", &limit);
-
-    while (a <= limit) {
-        printf("%d", a);
-        next = a + b;
-        a = b;
-        b = next;
-        if (a <= limit)
-            printf(" ");
-    }
-    printf("\n");
-    return 0;
-}
