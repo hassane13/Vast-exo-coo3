9f022ef487c61f93de5f20ea6379300bfb8549da
Votre Nom <ummihamidou@gmail.com>
2025-05-18 22:33:10 +0100

    or

--- reverse_number.c
+++ reverse_number.c
@@ -4,26 +4,12 @@ int main() {
     int n, reversed = 0;
     scanf("%d", &n);
 
-    int negatif = 0;
-    if (n < 0) {
-        negatif = 1;
-        n = -n; // Prendre la valeur absolue pour inverser
-    }
-
-    if (n == 0) {
-        reversed = 0;
-    } else {
-        while (n != 0) {
-            reversed = reversed * 10 + (n % 10);
-            n /= 10;
-        }
-    }
-
-    if (negatif) {
-        reversed = -reversed; // Remettre le signe négatif si besoin
+    // Supposons n >= 0
+    while (n != 0) {
+        reversed = reversed * 10 + (n % 10);
+        n /= 10;
     }
 
     printf("Nombre inversé : %d\n", reversed);
-
     return 0;
 }
