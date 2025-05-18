fc9813306be1aae18797b494940b94ff83397bb8
Votre Nom <ummihamidou@gmail.com>
2025-05-18 23:29:45 +0100

    first

--- reverse_number.c
+++ reverse_number.c
@@ -4,26 +4,11 @@ int main() {
     int n, reversed = 0;
     scanf("%d", &n);
 
-    int negatif = 0;
-    if (n < 0) {
-        negatif = 1;
-        n = -n;
-    }
-
-    if (n == 0) {
-        reversed = 0;
-    } else {
-        while (n != 0) {
-            reversed = reversed * 10 + (n % 10);
-            n /= 10;
-        }
-    }
-
-    if (negatif) {
-        reversed = -reversed;
+    while (n != 0) {
+        reversed = reversed * 10 + (n % 10);
+        n /= 10;
     }
 
     printf("Nombre inversé : %d\n", reversed);
-    
     return 0;
 }
