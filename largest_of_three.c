d04199f99711ce86140f7a7e8455b1258aeda8b1
Votre Nom <ummihamidou@gmail.com>
2025-05-18 20:28:16 +0100

    fuir

--- largest_of_three.c
+++ largest_of_three.c
@@ -2,15 +2,14 @@
 
 int main() {
     int a, b, c;
-
     scanf("%d %d %d", &a, &b, &c);
 
     if (a >= b && a >= c) {
-        printf("Le plus grand nombre est : %d\n", a);
+        printf("%d\n", a);
     } else if (b >= a && b >= c) {
-        printf("Le plus grand nombre est : %d\n", b);
+        printf("%d\n", b);
     } else {
-        printf("Le plus grand nombre est : %d\n", c);
+        printf("%d\n", c);
     }
 
     return 0;
