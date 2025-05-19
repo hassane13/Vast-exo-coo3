8087a491ef1467aaa374e3ccc806d975998cbeaa
Votre Nom <ummihamidou@gmail.com>
2025-05-19 02:07:46 +0100

    oust

--- largest_of_three.c
+++ largest_of_three.c
@@ -3,19 +3,14 @@
 int main() {
     int a, b, c;
 
-    // Lire trois entiers depuis l'entrée standard
     scanf("%d %d %d", &a, &b, &c);
 
-    // Comparer les trois nombres pour trouver le plus grand
     if (a >= b && a >= c) {
-        // a est plus grand ou égal aux deux autres
-        printf("Le plus grand nombre est : %d\n", a);
+        printf("%d\n", a);
     } else if (b >= a && b >= c) {
-        // b est plus grand ou égal aux deux autres
-        printf("Le plus grand nombre est : %d\n", b);
+        printf("%d\n", b);
     } else {
-        // c est le plus grand
-        printf("Le plus grand nombre est : %d\n", c);
+        printf("%d\n", c);
     }
 
     return 0;
