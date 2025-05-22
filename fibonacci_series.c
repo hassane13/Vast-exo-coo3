99aaad43ba7748d7551334e2383247324219be09
Votre Nom <ummihamidou@gmail.com>
2025-05-22 05:01:01 +0100

    fibona

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,33 +1,24 @@
 #include <stdio.h>
 
-// Affiche la suite de Fibonacci jusqu'à une limite donnée
-
 int main() {
     int limit, a = 0, b = 1, next;
-    int first = 1;
-    int printed = 0;  // indique si au moins un nombre a été affiché
+    int printed = 0; // indique si un nombre a été affiché
 
-    // Lire la limite
     scanf("%d", &limit);
 
-    // Cas négatif : ne rien afficher, même pas \n
-    if (limit < 0) {
-        return 0;
-    }
-
     while (a <= limit) {
-        if (first) {
-            printf("%d", a);
-            first = 0;
-        } else {
-            printf(" %d", a);
+        if (printed) {
+            printf(" ");
         }
+        printf("%d", a);
         printed = 1;
+
         next = a + b;
         a = b;
         b = next;
     }
 
+    // Affiche un saut de ligne uniquement si quelque chose a été imprimé
     if (printed) {
         printf("\n");
     }
